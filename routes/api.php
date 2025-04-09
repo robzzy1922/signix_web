69647b8f536018f899e57ebe5013733351f869f1
Gamma Estu Mahardika <gammaestu05@gmail.com>
2025-04-09 13:14:37 +0700

    WIP: perubahan di api.php sebelum pull

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\OrmawaAuthController;
 use App\Http\Controllers\Api\DocumentController;
+use App\Http\Controllers\Api\DosenController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/ormawa/login', [OrmawaAuthController::class, 'login']);
+
+// Route untuk mengambil data dosen (tanpa autentikasi)
+Route::get('/dosen', [DosenController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/ormawa/logout', [OrmawaAuthController::class, 'logout']);
